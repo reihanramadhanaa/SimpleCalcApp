a2577315bd7db06285403078f47ff6302139aa23
Ramadhana <ramadhanaa44@gmail.com>
2025-04-11 21:00:17 +0700

    first commit

---
+++ SimpleCalcApp/Kalkulator.cs
@@ -0,0 +1,6 @@
+﻿public class Kalkulator
+{
+    public int Tambah(int a, int b) => a + b;
+
+    public int Kurang(int a, int b) => a - b;
+}
---
+++ SimpleCalcApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Kalkulator kalkulator = new Kalkulator();
+
+        Console.Write("Masukkan angka pertama: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Masukkan angka kedua: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Hasil Penjumlahan: " + kalkulator.Tambah(a, b));
+        Console.WriteLine("Hasil Pengurangan: " + kalkulator.Kurang(a, b));
+    }
+}
